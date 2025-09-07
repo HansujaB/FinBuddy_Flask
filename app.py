20882bdf3a1af0aa0c3955b3b407c16997e2c108
HansujaB <hansujaigdtuwcseai@gmail.com>
2025-09-07 12:40:40 +0530

    port changes

--- app.py
+++ app.py
@@ -237,5 +237,5 @@ def test_categorizer():
         return jsonify({"error": "No text provided"}), 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
